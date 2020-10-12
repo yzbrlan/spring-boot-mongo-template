e5a04aeb68c7081c3be162bcd1a3487ba57007ea
xywu <xiya.wu@foxmail.com>
2020-10-12 19:40:59 +0800

    feat(swagger)：添加swagger

---
+++ src/main/java/xywu/top/edu/common/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package xywu.top.edu.common;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author xywu
+ * @date 2020/10/12
+ */
+@Configuration //说明这是一个配置类
+@EnableSwagger2// 该注解开启Swagger2的自动配置
+public class SwaggerConfig {  //随便的一个类名
+
+}
